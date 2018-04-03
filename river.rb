5846b10ffcc13d72139ea87ae65b60b50c3e14d6
victor chugbo <vcmails2010@gmail.com>
2018-04-03 17:04:57 +0100

    corrections done

--- river.rb
+++ river.rb
@@ -1,9 +1,14 @@
-class Riv
-  def initialize(name)
+class River
+  def initialize(name, fish_count)
     @name = name
+    @fish_count = fish_count
   end
 def number_of_fish()
-  @fish_count.count()[]
+  @fish_count.count()
 end
 
+def lose_a_fish
+  @fish_count.pop()
+  return @fish_count.count()
+end
 end
--- specs/river_specs.rb
+++ specs/river_specs.rb
@@ -1,4 +1,4 @@
-equire("minitest/autorun")
+require("minitest/autorun")
 require("minitest/rg")
 
 require_relative("../river.rb")
@@ -6,18 +6,16 @@ require_relative("../river.rb")
 class RiverTest < MiniTest::Test
 def setup()
   fish_count = ["fish", "fish", "fish"]
-  @river=River.new("Clyde", fish_count)
+  @river = River.new("Clyde", fish_count)
 
 end
 
 def test_fish_count()
-  assert_equal(3, @river.number_of_fish)
+  assert_equal(3, @river.number_of_fish())
 
 end
 
 def test_lose_fish()
-  fish_count.pop
-  assert_equal(2, @river.number_of_fish())
-
+    assert_equal(2, @river.lose_a_fish())
 end
 end
